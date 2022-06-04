933036a3dafee97f799648d8153d02090f9c7bb8
Tyler McMichael <103290988+tyler308956@users.noreply.github.com>
2022-06-04 15:19:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 print("Welcome to the tip calculator!")
-bill_total = float(input("How much was the total bill? (Enter amount without currency symbol) ")
-bill_total_as_float = float(bill_total)
-print(bill_total_as_float)
-number_in_party = input("How many persons will split the bill? ")
-number_in_party_as_int = int(number_in_party)
-calculation = bill_total_as_float / number_in_party_as_int
-calculation_with_tip = round(calculation * 1.12, 2)
-print(f"Each person should pay: ${calculation_with_tip}")
+bill_total = float(input("How much was the total bill? $"))
+tip = int(input("How much tip would you like to give? 12, 15, or 20 (Do not add percentage sign) "))
+number_in_party = int(input("How many persons will split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = bill_total * tip_as_percent
+bill_total_with_tip = bill_total + total_tip_amount
+bill_total_per_person = bill_total_with_tip / number_in_party
+final_amount = round(bill_total_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
